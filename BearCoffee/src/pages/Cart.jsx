b63a717e55dfdd534233d0ff2e9c0118e5dcc280
swechha <Swechhashrestha8@gmail.com>
2026-03-05 11:05:58 -0800

    Added style to the cart

--- BearCoffee/src/pages/Cart.jsx
+++ BearCoffee/src/pages/Cart.jsx
@@ -1,4 +1,5 @@
 import Header from '../components/Header'
+import './Cart.css'
 
 export function Cart({ cartNumber, setCartNumber, cartItems, setCartItems }) {
     return (
@@ -7,18 +8,18 @@ export function Cart({ cartNumber, setCartNumber, cartItems, setCartItems }) {
             {cartItems.map((cartItem) => {
                 return (
                     <>
-                        <div className="menuItem" key={cartItem.id}>
-                            <div className="menuItemContent">
-                                <p className="itemName"> {cartItem.name}</p>
-                                <p className="itemDescription">{cartItem.description}</p>
-                                <p className="itemPrice">{cartItem.price}</p>
-                                <p>qty:{cartItem.qty}</p>
+                        <div className="cart-container" key={cartItem.id}>
+                            <div className="cart-item-content">
+                                <p className="item-name"> {cartItem.name}</p>
+                                <p className="item-description">{cartItem.description}</p>
+                                <p className="item-price">{cartItem.price}</p>
+                                <p className="item-quantity">qty:{cartItem.qty}</p>
                             </div>
                         </div>
                     </>
                 )
             })}
-            <button onClick={() => {
+            <button className="empty-cart-button" onClick={() => {
                 setCartNumber(0);
                 setCartItems([]);
             }}>Empty Cart</button>
